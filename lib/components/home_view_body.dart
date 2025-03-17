f56d6191b2854dae85008a3e0ceb7a665f68f05d
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-17 16:39:36 +0200

    create currency item

--- lib/components/home_view_body.dart
+++ lib/components/home_view_body.dart
@@ -28,7 +28,7 @@ class HomeViewBody extends StatelessWidget {
                     color: Colors.grey[700],
                   ),
                 ),
-                const Spacer(flex: 2),
+                const Spacer(),
                 Text(
                   'Price(USD)',
                   style: TextStyle(
@@ -38,7 +38,76 @@ class HomeViewBody extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 50,
+          ),
+          CurrencyItem()
+        ],
+      ),
+    );
+  }
+}
+
+class CurrencyItem extends StatelessWidget {
+  const CurrencyItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      height: MediaQuery.of(context).size.height / 16.5,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.grey,
+        ),
+      ),
+      child: Row(
+        children: [
+          Image.network(
+            'https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400',
+            scale: 9.5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0),
+            child: Text(
+              'Bitcoin',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: (MediaQuery.of(context).size.width - 27) / 3,
+            child: Center(
+              child: Text(
+                'BTC',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: (MediaQuery.of(context).size.width - 27) / 3,
+            child: Center(
+              child: Text(
+                r'12355421$',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
