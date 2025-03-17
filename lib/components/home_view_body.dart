1b830004ebb75913622a1d5930cf9e0184f21052
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-17 15:52:08 +0200

    create categories

--- lib/components/home_view_body.dart
+++ lib/components/home_view_body.dart
@@ -5,6 +5,42 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              children: [
+                Text(
+                  'Name',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const Spacer(),
+                Text(
+                  'Symbol',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const Spacer(flex: 2),
+                Text(
+                  'Price(USD)',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
